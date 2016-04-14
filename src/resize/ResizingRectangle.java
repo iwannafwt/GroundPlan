9166aa8f998f3c4cbc724eb22c2eff26babe78d9
iwannafwt <iwanna.fwtiadoy@gmail.com>
2016-04-14 20:32:53 +0300

    Add Split Panel in JFrame

--- src/resize/ResizingRectangle.java
+++ src/resize/ResizingRectangle.java
@@ -1,8 +1,9 @@
 package resize;
 
-import java.awt.Color;
+import java.awt.BorderLayout;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JSplitPane;
 import mouse.MouseAdapterForMoving;
 import mouse.MouseAdapterForResize;
 
@@ -11,19 +12,27 @@ import mouse.MouseAdapterForResize;
  * @author ΙΩΑΝΝΑ
  */
 public class ResizingRectangle extends JFrame{
+    protected JSplitPane split;
+    
     public ResizingRectangle()  {
         
         initUI();
     }
     
     private void initUI() {
-        IResize IR = new Resize(250,50,150,100);
+        IResize IR = new Resize(150,50,150,100);//diastaseis apo ta tetragwnakia
         
         IR.registerListeners(new MouseAdapterForResize(IR));
         IR.registerListeners(new MouseAdapterForMoving(IR));
         
         if(IR instanceof JPanel){
-            add((JPanel)IR);
+            
+        split = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, new JPanel(), (JPanel)IR);
+        split.setContinuousLayout(false);
+        split.setOneTouchExpandable(false);
+
+        getContentPane().add(split, BorderLayout.CENTER);
+
         }else {
             System.out.println("An Error occured when trying registering the IResize.");
             System.exit(2);
@@ -33,7 +42,9 @@ public class ResizingRectangle extends JFrame{
         setTitle("Resize rectangle");
         setSize(800, 800);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setLocationRelativeTo(null);       
+        setLocationRelativeTo(null);    
+        
+        
     }
     
 }
