2735d2d67b166e840ffcef3b99f1b56bce46558e
iwannafwt <iwanna.fwtiadoy@gmail.com>
2016-04-23 23:39:44 +0300

    Create a rectangle Class to make a rectangle

--- src/CanvasToDraw/WithShape/shape/Rectangle.java
+++ src/CanvasToDraw/WithShape/shape/Rectangle.java
@@ -96,32 +96,19 @@ public class Rectangle  extends JPanel implements IRectangle  {
         Graphics2D g2 = (Graphics2D) g;
 
         g2.setColor(Color.red);//to xrwmma apo ta mikra ta tetragwnakia
-        Rectangle2D r = new Rectangle2D.Double();
-        
-        for (Point2D point : points) {//to mikro tetragwno
-            //einai to kentro tou mikrou tetragwnou
-            double x = point.getX() - SIZE / 2;
-            double y = point.getY() - SIZE / 2;
-            r.setFrame(x, y, SIZE, SIZE);//to Frame tou orizei ti megethos tha exei
-            g2.fill(r);
-        }
-        
-        for(int i =0 ; i < points.length ; i = i + 2 ){ //me ti kathe epanalipsi 
-                                                        //dimiourgite kai ena tetragwno.gi auto einai ana 2         
-            r.setFrameFromDiagonal(points[i], points[i+1]); 
-            g2.setPaint(new Color(0, 0, 100));// to xrwmma pou tha exei to tetragwno
-            g2.fill(r);//gemizei to tetragwno
-            g2.draw(r);//kanei to perigramma tou tetragwnou
         
         setBackground(Color.gray);
+        
+        MakeRectangle makeRec = new MakeRectangle();
+        makeRec.makeRec(points, g);
          
         Background backGround = new Background();
         
         backGround.backgroundColour(1500, 1500, 80 , 80, g2);
         
+        
         }
-    }
-    
+
     
     @Override
     public void paintComponent(Graphics g) {
